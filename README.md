6044370f77d3fc1c9e3afc9db0e3eaaf8e75937f
Hanying Chen <chenhanying940528@gmail.com>
2021-07-20 23:53:06 -0400

    Update readme; added parsed data in csv; skip if headline is None

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # covid-news-stock
+
+## Instruction
+1. Set up local conda environment using the `environment.yaml`
+```bash
+conda env create --file=environment.yaml
+```
+2. To run CNN crawler, go to `news/` folder and run `scrapy crawl CNNNews`.
--- news/news/spiders/cnn_spider.py
+++ news/news/spiders/cnn_spider.py
@@ -6,7 +6,7 @@ BASE_URL = "https://search.api.cnn.io"
 class CNNNews(scrapy.Spider):
     name = "CNNNews"
     start = 1
-    start_urls = [BASE_URL + "/content?q=covid-19&sort=newest&size=100"]
+    start_urls = [BASE_URL + "/content?q=covid&size=100"]
 
     def parse(self, response):
         # Get all the news summary from the current page
@@ -16,12 +16,13 @@ class CNNNews(scrapy.Spider):
         for res in results:
             headline = res['headline']
             # Only if headline contains the following keywords
-            if 'covid' in headline.lower() or 'coronavirus' in headline.lower() \
-                    or 'pandemic' in headline.lower() or 'sars' in headline.lower()\
-                    or 'vaccination' in headline.lower() or 'virus' in headline.lower():
-                dt = res['firstPublishDate'][:10]  # we only need the date
-                dates.append(dt)
-                headlines.append(headline)
+            if headline:
+                if 'covid' in headline.lower() or 'coronavirus' in headline.lower() \
+                        or 'pandemic' in headline.lower() or 'sars' in headline.lower()\
+                        or 'vaccination' in headline.lower() or 'virus' in headline.lower():
+                    dt = res['firstPublishDate'][:10]  # we only need the date
+                    dates.append(dt)
+                    headlines.append(headline)
         page = response.url.split("/")[-2]
         filename = f'{self.name}-{page}.txt'
 
@@ -33,6 +34,6 @@ class CNNNews(scrapy.Spider):
 
         # Next page
         self.start += 100
-        next_page_url = BASE_URL + f"/content?q=covid-19&sort=newest&size=100&from={self.start}"
+        next_page_url = BASE_URL + f"/content?q=covid&size=100&from={self.start}"
         self.log("Crawling next page...")
         yield scrapy.Request(next_page_url, callback=self.parse)
